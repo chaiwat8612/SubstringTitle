ac3063c25c5b8caa36a126f374bb9c3912d2a1bf
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2021-09-24 12:32:18 +0700

    Add count function

---
+++ SubstringTitle/wwwroot/js/title.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    //original text
+    var text = '"สมใจปรารถนา" ผลงานชนะเลิศจากกิจกรรม Umay+ MONEY FITNESS';
+
+    document.getElementById("original").innerHTML = text;
+
+    //count uppercase
+    var cnt = countUpperCase(text);
+    document.getElementById("count").innerHTML = cnt.toString();
+
+})
+
+function countUpperCase(str) {
+
+    var cnt = str.length - str.replace(/[A-Z]/g, '').length;
+
+    return cnt;
+
+}
